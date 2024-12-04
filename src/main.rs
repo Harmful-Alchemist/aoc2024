5de75c943e3e6b6f325db31814c9e12a2ee5a25f
harm <el.harmo@gmail.com>
2024-12-04 13:49:15 +0100

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,100 @@
-use std::{iter};
+use std::{array, char, iter, time::Instant};
 
 fn main() {
+    let now = Instant::now();
     day_one();
+    let elapsed = now.elapsed();
+    println!("day 1: {elapsed:?}");
+
+    let now = Instant::now();
     let res = day_three_one(&std::fs::read_to_string("input/day3.txt").unwrap());
-    println!("{res}");
+    let elapsed = now.elapsed();
+    println!("day 3 1: {elapsed:?} {res}");
+    let now = Instant::now();
     let res = day_three_two(&std::fs::read_to_string("input/day3.txt").unwrap());
-    println!("{res}");
+    let elapsed = now.elapsed();
+    println!("day 3 2: {elapsed:?} {res}");
+
+    let now = Instant::now();
+    let res = day_four_one(&std::fs::read_to_string("input/day4.txt").unwrap());
+    let elapsed = now.elapsed();
+    println!("day 4 1: {elapsed:?} {res}");
+
+    let now = Instant::now();
+    let res = day_four_two(&std::fs::read_to_string("input/day4.txt").unwrap());
+    let elapsed = now.elapsed();
+    println!("day 4 2: {elapsed:?} {res}");
+}
+
+fn day_four_one(inp: &str) -> usize {
+    let to_find = ['X', 'M', 'A', 'S'];
+    let cs: Vec<char> = inp.chars().collect();
+    let line_len: i32 = line_len(&cs);
+
+    let checks = [
+        [0, 1, 2, 3],
+        array::from_fn(|x| -(x as i32)),
+        array::from_fn(|x| x as i32 * line_len),
+        array::from_fn(|x| -(x as i32) * line_len),
+        array::from_fn(|x| x as i32 * line_len + x as i32),
+        array::from_fn(|x| x as i32 * line_len - x as i32),
+        array::from_fn(|x| x as i32 * -line_len + x as i32),
+        array::from_fn(|x| x as i32 * -line_len - x as i32),
+    ];
+
+    (0..cs.len())
+        .map(|i| {
+            checks
+                .iter()
+                .filter(|check| {
+                    check.iter().zip(to_find.iter()).all(|(j, c)| {
+                        i as i32 + j > -1 && cs.get((i as i32 + j) as usize) == Some(c)
+                    })
+                })
+                .count()
+        })
+        .sum()
+}
+
+fn day_four_two(inp: &str) -> usize {
+    let cs: Vec<char> = inp.chars().collect();
+    let line_len: i32 = line_len(&cs);
+    let (right_top, left_top, right_bottom, left_bottom) =
+        (-line_len - 1, -line_len + 1, line_len - 1, line_len + 1);
+
+    (0..cs.len())
+        .filter(|i| {
+            cs.get(*i) == Some(&'A')
+                && is_good_pos(*i, line_len)
+                && *i as i32 + right_top > -1
+                && check_mas(*i, left_top, right_bottom, &cs)
+                && check_mas(*i, right_top, left_bottom, &cs)
+        })
+        .count()
+}
+
+fn line_len(cs: &[char]) -> i32 {
+    for (i, c) in cs.iter().enumerate() {
+        if *c == '\n' {
+            return i as i32 + 1;
+        }
+    }
+    panic!()
+}
+
+fn check_mas(i: usize, mod1: i32, mod2: i32, cs: &[char]) -> bool {
+    matches!(
+        (
+            cs.get((i as i32 + mod1) as usize),
+            cs.get((i as i32 + mod2) as usize)
+        ),
+        (Some('M'), Some('S')) | (Some('S'), Some('M'))
+    )
+}
+
+fn is_good_pos(i: usize, line_len: i32) -> bool {
+    let i = i as i32;
+    i % line_len != 0 && i % line_len != line_len - 1
 }
 
 fn day_three_two(inp: &str) -> i32 {
@@ -16,7 +105,7 @@ fn day_three_two(inp: &str) -> i32 {
         .map(|s| s.split("do()").collect())
         .for_each(|split: Vec<&str>| {
             for s in &split[1..] {
-                new_str.push_str(*s);
+                new_str.push_str(s);
             }
         });
     day_three_one(&new_str)
@@ -107,6 +196,39 @@ mod tests {
     use super::*;
 
     #[test]
+    fn day_four() {
+        let result = day_four_one(
+            "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX",
+        );
+
+        assert_eq!(result, 18);
+
+        let result = day_four_two(
+            ".M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........",
+        );
+
+        assert_eq!(result, 9);
+    }
+
+    #[test]
     fn day_three() {
         let result = day_three_one(
             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
@@ -155,7 +277,7 @@ fn day_one() {
             let x = *x as usize;
             x * counts[x]
         })
-        .fold(0, |acc, x| acc + x);
+        .sum::<usize>();
 
     println!("{lol}")
 }
